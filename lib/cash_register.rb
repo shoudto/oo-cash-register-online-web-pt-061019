0e8bacd8c63aa6a856dfcb4eb4659caccb0a19fc
Oscar Shoudt <shoudto@gmail.com>
2019-08-05 19:43:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,9 +5,10 @@ class CashRegister
   def initialize(total = 0, discount = 0)
     @total = total
     @discount = discount
+    @items = []
   end
 
   def add_item(title, price, quantity = 1)
-
+    
   end
 end
